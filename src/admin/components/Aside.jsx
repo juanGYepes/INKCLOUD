102e00613ea5877dc393fb6abd5a46bc11e4e999
Camilito05 <atehortuaflorezc@gmail.com>
2025-06-02 19:21:00 -0500

    creacin de las rutas en el el dashboar para crear y  hacer reportes entre otros

--- src/admin/components/Aside.jsx
+++ src/admin/components/Aside.jsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom"
+import { linksNavigate } from "../../components/auth/common/getLinkNavegate"
+import { useState } from "react"
+import { useAuth } from "../../context/AuthContext"
 
 export default function Aside() {
-  return (
-    <>
-      <ul className="navbar-nav bg-warning sidebar sidebar-dark accordion" id="accordionSidebar">
+    let { user } = useAuth();
+    let role = user.rol;
+    let roleRoutes = role ? [...(linksNavigate[role] || []), ...linksNavigate.all] : [];
 
-           {/*<!-- Sidebar - Brand -->*/}
+    let [openSubMenu,setOpensetMenu] = useState(null);
+    let toggleMenu =(label)=>{
+        setOpensetMenu(openSubMenu === label ? null : label);
+    }
+
+    return (
+        <>
+        <ul className="navbar-nav bg-warning sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            {/*<!-- Sidebar - Brand -->*/}
             <Link className="sidebar-brand d-flex align-items-center justify-content-center" to="/dashboard">
                 <div className="sidebar-brand-icon rotate-n-15">
                     <i className="fas fa-laugh-wink"></i>
@@ -16,7 +28,7 @@ export default function Aside() {
             {/*<!-- Divider -->*/}
             <hr className="sidebar-divider my-0"/>
 
-           {/*<!-- Nav Item - Dashboard -->*/}
+            {/*<!-- Nav Item - Dashboard -->*/}
             <li className="nav-item active">
                 <Link className="nav-link" to="/dashboard">
                     <i className="fas fa-fw fa-tachometer-alt"></i>
@@ -31,38 +43,40 @@ export default function Aside() {
                 Modulos
             </div>
 
-            {/*<!-- Nav Item - Pages Collapse Menu -->*/}
-            <li className="nav-item">
-                <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fa-solid fa-book"></i>   
-                    <span>Libros</span>
-                </a>
-                <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div className="bg-white py-2 collapse-inner rounded">
-                        <h6 className="collapse-header">Acciones:</h6>
-                        <a className="collapse-item" href="listado-pro.html">Ver Libros</a>
-                        <a className="collapse-item" href="crear-pro.html">Crear Libro</a>
-                    </div>
-                </div>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fa-solid fa-layer-group"></i>  
-                    <span>Categorias</span>
-                </a>
-                <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div className="bg-white py-2 collapse-inner rounded">
-                        <h6 className="collapse-header">Acciones:</h6>
-                        <a className="collapse-item" href="listado-pro.html">Ver categorias</a>
-                        <a className="collapse-item" href="crear-pro.html">Crear Categoria</a>
-                    </div>
-                </div>
-            </li>
+            {
+                roleRoutes.map((link , i) => (
+                    link.children ? (
+                        <li className="nav-item" key={i}>
+                            <button onClick={() => toggleMenu(link.label)}
+                                className="nav-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo"
+                                aria-expanded={openSubMenu ===link.label ? "true" : "false"} aria-controls="collapseTwo">
+                                <i className={link.icon}></i>   
+                                <span>{link.label}</span>
+                            </button>
+                            <div id="collapseTwo" className={ `collapse ${openSubMenu === link.label  ? "show" : ""} `} aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                                <div className="bg-white py-2 collapse-inner rounded">
+                                    <h6 className="collapse-header">Acciones:</h6>
+                                    {
+                                        link.children.map((subLink , subIndex)=>(
+                                            <Link className="collapse-item" to={subLink.path} key={subIndex}>
+                                                {subLink.label}
+                                            </Link>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        </li>
+                    ) : (
+                        <li className="nav-item" key={i}>
+                            <Link className="nav-link" to={link.path}>
+                                <i className={link.icon}></i>   
+                                <span>{link.label}</span>
+                            </Link>
+                        </li>
+                    )
+                ))
+            }
 
-
-            {/*<!-- Divider -->*/}
             <hr className="sidebar-divider"/>
 
             {/*<!-- Sidebar Toggler (Sidebar) -->*/}
@@ -71,6 +85,6 @@ export default function Aside() {
             </div>
 
         </ul>
-    </>
-  )
+        </>
+    )
 }
--- src/components/auth/common/getLinkNavegate.jsx
+++ src/components/auth/common/getLinkNavegate.jsx
@@ -1,7 +1,78 @@
 import React from 'react'
 
-export default function getLinkNavegate() {
-  return (
-    <div>getLinkNavegate</div>
-  )
+export  const linksNavigate ={
+    "admin":[
+        {
+           "label":"libros",
+           "icon":"fa-solid fa-book",
+           "children":[
+                {"path":"/dashboard/libros","label":"Ver Libros"},
+                {"path":"/dashboard/libros/crear" , "label":"crear libro"}
+            ]
+           
+        },
+        {
+           "label": "post",
+           "icon": "fa-solid fa-pen-to-square",
+           "children":[
+                {"path":"/dashboard/post","label":"Ver publicaciones"},
+                {"path":"/dashboard/post/crear" , "label":"crear publicaciones"}
+            ]
+           
+        },
+          {
+           "label":"users",
+           "icon":"fa-solid fa-user",
+           "children":[
+                {"path":"/dashboard/users","label":"Ver usuarios"},
+                {"path":"/dashboard/users/crear" , "label":"crear usurios"}
+            ]
+           
+        },  
+        {
+           "label":"reports",
+           "icon":"fa-solid fa-chart-line",
+           "children":[
+                {"path":"/dashboard/reports","label":"Ver reportes"},
+                {"path":"/dashboard/reports/crear" , "label":"crear reportes"}
+            ]
+           
+        },
+          {
+           "label":"permissions",
+           "icon":"fa-solid fa-table-cells-row-unlock",
+           "children":[
+                {"path":"/dashboard/permissions","label":"Ver permisos"},
+                {"path":"/dashboard/permissions/crear" , "label":"crear permisos"}
+            ]
+           
+        },
+        
+        /*rutas no hijas  
+          {
+            "label":"profile",
+           "icon":"fa-solid fa-id-badge",
+           "path":"/dashboard/profile"
+            
+           
+        }*/
+    ],
+    "customer":[
+        {
+            "label":"orders",
+            "icon":"fa-solid fa-bag-shopping",
+            "path":"/dashboard/orders"
+        }
+      
+    ],
+    "all":[
+        {
+           "label":"profile",
+           "icon":"fa-solid fa-id-badge",
+           "path":"/dashboard/profile"
+            
+           
+        }
+    ]
+    
 }
