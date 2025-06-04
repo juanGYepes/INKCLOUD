8f39aa3fd99bb02b56a813644fe253d9f1a7819f
juanGYepes <juancyepes97@gmail.com>
2025-06-03 21:11:55 -0500

    Modificacion al componente Aside del Front

--- src/frontend/components/Aside.jsx
+++ src/frontend/components/Aside.jsx
@@ -5,11 +5,29 @@ export default function Aside() {
     <>
       <aside className="col-md-3 my-3">
       {/* Aquí van filtros, categorías, etc */}
-        <h5>Categorías</h5>
+        <h5>RECOMENDADOS DE LA SEMANA</h5>
         <ul className="list-group">
-          <li className="list-group-item">Crecimiento Personal</li>
-          <li className="list-group-item">Ficción</li>
-          <li className="list-group-item">No Ficción</li>
+          <li className="list-group-item">
+            <strong>Título:</strong><br />
+            <strong>Author:</strong>
+          </li>
+          <li className="list-group-item">
+            <strong>Título:</strong><br />
+            <strong>Author:</strong>
+          </li>
+          <li className="list-group-item">
+            <strong>Título:</strong><br />
+            <strong>Author:</strong>
+          </li>
+          <li className="list-group-item">
+            <strong>Título:</strong><br />
+            <strong>Author:</strong>
+          </li>
+          <li className="list-group-item">
+            <strong>Título:</strong><br />
+            <strong>Author:</strong>
+          </li>
+          
         </ul>
       </aside>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-//import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = createRoot(document.getElementById('root'))
 root.render(
